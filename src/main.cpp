7fe559b052129cd5bc729c965685c920542dff06
S4kyt <jerikolo1@gmail.com>
2020-11-04 17:06:33 +0100

    Moved source files to src cuz forgot

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,73 @@
-// Pointer
-// IOL: 
+// This project contains every single subject within Section 5 of the course.
 
 #include <iostream>
+#include <limits>
 
-void myFunction(double* p_value)
-{
-	double funcValue = 66.6;
-	p_value = &funcValue;
-
-	std::cout << "Pointer's function value: " << *p_value << std::endl;
-	std::cout << "Pointer's function address: " << &funcValue << std::endl;
-}
+#include "main.h"
+#include "util.h"
 
 int main()
 {
-	// 1. Type out value,make it double, give it a value
-	double value = 22.2;
-	double* p_value = &value;
-	
-	std::cout << "Value: " << value << std::endl;
-	std::cout << "Default pointer's memory address: " << p_value << std::endl;
+	bool browsingSubjects = true;
 
-	// 3. Try using the pointer to change its value
-	double newValue = 44.4;
-	p_value = &newValue;
+	std::string menuInput = "";
 
-	std::cout << "Pointer's new value: " << *p_value << std::endl;
-	std::cout << "Pointer's new address: " << p_value << std::endl;
+	while (browsingSubjects == true)
+	{
+		clearScreen();
+		std::cout << "Choose which subject's results you'd like to see: " << std::endl;
+		std::cout << "1. Pointer" << std::endl;
+		std::cout << "2. Unavailable" << std::endl;
+		std::cout << "3. Unavailable" << std::endl;
+		std::cout << "4. Exit program" << std::endl;
 
-	// 4. Then declare function that takes a pointer as parameter,
-	//    and try passing our value's address to that function,
-	//    try changing it there and see what I get.
-	myFunction(&newValue);
+		std::cin >> menuInput;
 
-	system("pause");
+		// Subjects:
+		if (menuInput == "1")
+		{
+			// Pointer
+			clearScreen();
+			pointerPractice();
+		}
+		else if (menuInput == "2")
+		{
+			// Unavailable
+			clearScreen();
+			std::cout << "This is not available yet." << std::endl;
+		}
+		else if (menuInput == "3")
+		{
+			// Unavailable
+			clearScreen();
+			std::cout << "This is not available yet." << std::endl;
+		}
+		else if (menuInput == "4")
+		{
+			// Exit
+			browsingSubjects = false;
+			break;
+		}
+		else
+		{
+			// Yes, the message is barely see-able but thats a feature, not a bug! :D
+			std::cout << " Please choose a correct option from above." << std::endl;
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	}
+	return 0;
 }
+
+// Issue I ran into, and shouldn't forget about:
+//
+		// FIXME: My current issue is that after lets say, the user makes a mistake by entering something that throws us here to default,
+		//        default runs like it should, and since we are in the while loop, we continue from the beginning of the scope,
+		//        displaying each std::cout
+		//
+		//        The issue is that once it reaches std::cin, and it seems to skip it altogether?
+		//        My assumption is that it already took the value once, so it doesn't work the way I suppose it does
+		//        and it wont take menuInput again :(
+//
+// It was solved by changing menuInput from int to std::string because switch doesn't take string as its parameter,
+// and we get alot more freedom with if-elseif-else and string because the input can be whatever, saving us a workaround from having
+// to write error handling for the switch.
