13b1e1afe5a107dad92645aee46dc4ab70a43edc
S4kyt <jerikolo1@gmail.com>
2020-11-05 13:31:20 +0100

    Arithmetic - Sec to HrMinSec converter finished.

--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -1,30 +1,48 @@
+// Arithmetic
+// IMPORTANT: This project got comprimised because I found assignment operators (expect "=" ofc.) to be a nuisance to use,
+//			  Instead I finished this project without using them, and still completed a small program with it.
+//            The program is a Second - to - Hour,Minute,Second converter!
+//
+// Project description:
 // Take large numbers of seconds and convert it to hours, minutes, seconds
 // Write a for loop that iterates 10,000 times; output a dot (on the same line) every 100 iteration (use mod %)
 
 #include <iostream>
 
 #include "main.h"
+#include "util.h"
 
 void methicPractice()
 {
+	// Variable range: 0 to 4,294,967,295
+	using ulint = unsigned long int;
 
 	std::cout << "Please enter, possibly a large amount of seconds: " << std::endl;
 
-	int bigNumber = 0;
+	ulint bigNumber = 0;
 
 	std::cin >> bigNumber;
 
-	std::cout << "You've entered: " << bigNumber << std::endl;
+	// 1. Hours
+	ulint convHour = (bigNumber / 3600);
+	ulint hourRemainder = (bigNumber % 3600);
 
-	// Seemed important to start with hours, then continue with minutes and only after that convert to seconds.
+	std::cout << "\n";
+	std::cout << "Converted hours: " << (convHour * 3600) << std::endl;
+	std::cout << "Remainder from converted hours: " << hourRemainder << std::endl;
 
-	int convHour = bigNumber /= 3600;
-	int hourRemainder = bigNumber %= 3600;
+	// 2. Minutes
+	ulint convMinutes = (hourRemainder / 60);
+	ulint minuteRemainder = (hourRemainder % 60);
 
-	std::cout << bigNumber << std::endl;
-	std::cout << convHour << std::endl;
-	std::cout << hourRemainder << std::endl;
-	
+	std::cout << "Converted minutes: " << convMinutes << std::endl;
 
-	system("pause");
+	// 3. Seconds
+	std::cout << "Converted seconds: " << minuteRemainder << std::endl;
+
+	std::cout << "\n";
+	std::cout << "Hour, Minute, Second format: " << std::endl;
+	std::cout << convHour << " Hours " << convMinutes << " Minutes " << minuteRemainder << " Seconds " << std::endl;
+
+	toContinue();
 }
